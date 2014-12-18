886d8ccdfaa142d5c6b447645da8a5d0b40c18a9
Jonas Bhend <jonas.bhend@meteoswiss.ch>
2014-12-18 14:32:24 +0100

    Added licence statement

--- R/veriApply.R
+++ R/veriApply.R
@@ -1,4 +1,22 @@
-#' Apply verification metrics from Specs-Verification to large datasets
+# veriApply.R apply verification metrics to large datasets
+#
+#     Copyright (C) 2014 MeteoSwiss
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#' Apply verification metrics to large datasets
 #' 
 #' @param verifun Name of function to compute verification metric (score, skill score)
 #' @param fcst array of forecast values (at least 2-dimensional)
